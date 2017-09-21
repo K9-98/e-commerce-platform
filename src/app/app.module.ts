5d2e8fc68791009931a12ba401ffa96944bb876d
K9-98 <k9idas@gmail.com>
2017-09-21 19:09:18 +0200

    initial commit
    assignment 1 login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './bottom-banner/footer/footer.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { BottomBannerComponent } from './bottom-banner/bottom-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    TopNavComponent,
+    BottomBannerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bottom-banner/bottom-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-banner',
+  templateUrl: './bottom-banner.component.html',
+  styleUrls: ['./bottom-banner.component.css']
+})
+export class BottomBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
